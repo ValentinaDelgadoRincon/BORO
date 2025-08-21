f2cff80f3f45fedb8a087109ecf018acf68b5991
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-21 18:23:26 -0500

    feat: transacciones
    transaccion dodne se simula

---
+++ transacciones.js
@@ -0,0 +1,25 @@
+const session = db.getMongo().startSession();
+const dbSession = session.getDatabase("Boro");
+session.startTransaction();
+try{
+  dbSession.inventario.insertOne({
+    
+_id:11,
+productoId:11,
+lote:"L011",
+cantidad:25,
+entrada:new Date("2025-06-20")
+  });
+  dbSession.productos.updateOne(
+    {nombre:"Chocolatina"},
+    {$inc:{stock:25}}
+  );
+  session.commitTransaction();
+  print("producto agregado exitosamente")
+}catch(error){
+  session.abortTransaction();
+  print("producto no agregado")
+  print(error)
+}finally{
+  session.endSession();
+}
