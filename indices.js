e80046c97c74c1bcede71b1b6aa372cce53166a2
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-08-22 09:12:23 -0500

    feat: :sparkles: completar  indices
    completar indices
    
    By @Valentina

--- indices.js
+++ indices.js
@@ -4,3 +4,9 @@ db.clientes.createIndex({email:1})
 //Usar explain() en una consulta para mostrar si el índice de nombre está siendo utilizado.
 db.productos.createIndex({nombre:1})
 db.productos.find({nombre:"nombre_1"}).explain("executionStats")
+
+// 1. Crear un índice en el campo nombre de productos para mejorar búsquedas por nombre.
+db.productos.createIndex({nombre:1})
+
+// 2. Crear un índice compuesto sobre categoria y precio para facilitar búsquedas filtradas por ambas condiciones.
+db.productos.createIndex({categoria:1},{precio:1})
