9c196d39ff050eb2f35acb8f3d663a02a304a81b
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-08-22 08:52:43 -0500

    feat: :sparkles: completar transacciones
    completar transacciones
    
    By @Valentina

--- transacciones.js
+++ transacciones.js
@@ -27,3 +27,46 @@ entrada:new Date("2025-06-20")
 }finally{
   session.endSession();
 }
+
+// 1. Simular una venta:
+// a. Descontar del stock del producto
+// b. Insertar la venta en la colección `ventas`
+
+const descontar = db.getMongo().startSession();
+const dbDescontar = descontar.getDatabase("BORO");
+descontar.startTransaction();
+
+try {
+  dbDescontar.productos.updateOne({ _id: 1 }, { $inc: { stock: -8 } });
+  dbDescontar.ventas.insertOne({ producto_id: 1, cantidad: 8, fecha: new Date() });
+
+  descontar.commitTransaction();
+  print("Venta registrada correctamente");
+} catch (e) {
+  descontar.abortTransaction();
+  print("Error:", e);
+} finally {
+  descontar.endDescontar();
+} 
+
+
+// 3. Hacer una operación de devolución:
+// a. Aumentar el stock
+// b. Eliminar la venta correspondiente
+
+const aumentar = db.getMongo().startSession();
+const dbAumentar = aumentar.getDatabase("BORO");
+aumentar.startTransaction();
+
+try {
+  dbAumentar.productos.updateOne({ _id: 7 }, { $inc: { stock: 5 } });
+  dbAumentar.ventas.deleteOne({ producto_id: 7, cantidad: 5, fecha: new Date() });
+
+  aumentar.commitTransaction();
+  print("Devolucion registrada correctamente");
+} catch (e) {
+  aumentar.abortTransaction();
+  print("Error:", e);
+} finally {
+  aumentar.endAumentar();
+} 
