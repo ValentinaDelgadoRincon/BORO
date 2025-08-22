a134240ff35b63244696d0df4876f54d101a00b1
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-22 08:19:15 -0500

    fix: :bug: sintaxis
    sintaxis
    
    By@ Camila y Valentina

--- funciones.js
+++ funciones.js
@@ -26,10 +26,11 @@ calcularDescuento(100, 15);
 
 db.system.js.insertOne({
     _id: "clienteActivo",
-    value: new Code ("function(clienteId) {return db.compras.countDocuments({ clienteId: clienteId })3;}")
+    value: new Code ("function(clienteId) {return db.compras.countDocuments({ clienteId: clienteId }) > 3;}")
 });
 
 const f2 = db.system.js.findOne({ _id: "clienteActivo" });
 const clienteActivo = new Function("return " + f2.value.code)();
+const cliente=db.clientes.findOne({nombre:"Diana Suárez"});
 
-clienteActivo(clienteId); 
+clienteActivo(cliente.clienteId); 
