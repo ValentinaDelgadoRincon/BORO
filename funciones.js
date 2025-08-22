b34ae8d0488488d0b445b97b46ad79d6a11b3b91
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-21 19:00:28 -0500

    feat: :sparkles: funcion verificacion de stock
    funcion dodne se verifica si la canridad que estan pidiendo es suficiente segun el stock
    
    By@CamilaFlorez

---
+++ funciones.js
@@ -0,0 +1,10 @@
+db.system.js.insertOne({
+    _id:"verificarstock",
+    value: new Code("function(stock,cantidadDeseada){return stock >= cantidadDeseada}")
+});
+
+const f3=db.system.js.findOne({_id:"verificarstock"});
+const verificarstock=new Function("return "+f3.value.code)();
+const borojo=db.productos.findOne({nombre:"Aceite de borojó"});
+
+verificarstock(borojo.stock,20);
